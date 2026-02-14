c360ae48551c6b91afba063e6d59b29dd0ac06a5
notnibloo <notnibloo@gmail.com>
2026-02-14 15:47:33 +0100

    Dev deploy

--- src/deploy-command-dev.ts
+++ src/deploy-command-dev.ts
@@ -18,8 +18,12 @@ for (const file of files) {
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
 
-await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), {
-  body: commands,
-});
+await rest.put(
+  Routes.applicationGuildCommands(
+    process.env.CLIENT_ID!,
+    process.env.GUILD_ID!,
+  ),
+  { body: commands },
+);
 
 console.log("Commands deployed");
